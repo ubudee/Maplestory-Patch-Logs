80200e5a9b9b209bb290b489709c993603a4c0b6
Lurstan Rahim <l.rahim@ubudee.com>
2022-07-13 21:30:42 +0200

    Uploaded tool

---
+++ Maplestory Patch Logs/Models/HomeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Maplestory_Patch_Logs.Models
+{
+    public class HomeModel
+    {
+        public string? Html { get; set; }
+        public string? Github { get; set; }
+        public string? Version { get; set; }
+        public string? Date { get; set; }
+    }
+}
